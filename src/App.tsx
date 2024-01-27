29dcf99d2197fc959d074119f3a065c5ed4ee32b
ahlaulhee <ahlaulhe@gmail.com>
2024-01-27 19:19:32 -0300

    feat: Basic page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,165 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Bind, Keybind } from "./vite-env";
+
+const emptyKeybind: Keybind = {
+  action: "",
+  bind: {
+    ctrl: false,
+    super: false,
+    alt: false,
+    shift: false,
+    key: "",
+  },
+  category: "",
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [keybinds, setKeybinds] = useState<Keybind[]>([]);
+  const [keybind, setKeybind] = useState<Keybind>(emptyKeybind);
+
+  useEffect(() => {
+    // TODO: Load keybinds from LocalStorage
+    setKeybinds([]);
+  }, []);
+
+  const storeKeybind = () => {
+    if (!keybind.action || !keybind.category || !keybind.bind.key) return;
+    keybinds.push(keybind);
+    setKeybind(emptyKeybind);
+    // TODO: Save to LocalStorage
+    console.log(keybind);
+    console.log(keybinds);
+  };
+
+  const formatBind = (bind: Bind) => {
+    return `${bind.ctrl ? "Ctrl+" : ""}${bind.super ? "Super+" : ""}${
+      bind.alt ? "Alt+" : ""
+    }${bind.shift ? "Shift+" : ""}${bind.key}`;
+  };
+
+  const exportJSON = () => {
+    // TODO: Retrieve from localstorage and download .json
+  };
+
+  const importJSON = () => {
+    // TODO: Save .json data to localstorage and modify state
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <section>
+        <button className="block accent">Listen to keys</button>
+        <div className="wrapper block">
+          <input
+            type="text"
+            placeholder="Action..."
+            value={keybind.action}
+            onChange={(e) => setKeybind({ ...keybind, action: e.target.value })}
+          />
+        </div>
+        <div className="wrapper block">
+          <input
+            type="text"
+            placeholder="Category..."
+            value={keybind.category}
+            onChange={(e) =>
+              setKeybind({ ...keybind, category: e.target.value })
+            }
+          />
+        </div>
+        <CheckboxWithLabel
+          label="Control"
+          checked={keybind.bind.ctrl}
+          onChange={() =>
+            setKeybind({
+              ...keybind,
+              bind: { ...keybind.bind, ctrl: !keybind.bind.ctrl },
+            })
+          }
+        />
+        <CheckboxWithLabel
+          label="Super"
+          checked={keybind.bind.super}
+          onChange={() =>
+            setKeybind({
+              ...keybind,
+              bind: { ...keybind.bind, super: !keybind.bind.super },
+            })
+          }
+        />
+        <CheckboxWithLabel
+          label="Alt"
+          checked={keybind.bind.alt}
+          onChange={() =>
+            setKeybind({
+              ...keybind,
+              bind: { ...keybind.bind, alt: !keybind.bind.alt },
+            })
+          }
+        />
+        <CheckboxWithLabel
+          label="Shift"
+          checked={keybind.bind.shift}
+          onChange={() =>
+            setKeybind({
+              ...keybind,
+              bind: { ...keybind.bind, shift: !keybind.bind.shift },
+            })
+          }
+        />
+        <div className="wrapper block">
+          <input
+            type="text"
+            placeholder="Key..."
+            value={keybind.bind.key}
+            onChange={(e) =>
+              setKeybind({
+                ...keybind,
+                bind: { ...keybind.bind, key: e.target.value },
+              })
+            }
+          />
+        </div>
+        <button className="block accent" onClick={storeKeybind}>
+          Store Keybind
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
+      <section className="block">
+        {keybinds.map((keybind, i) => {
+          return (
+            <div className="block" key={i}>
+              {formatBind(keybind.bind)} | {keybind.action} | {keybind.category}
+            </div>
+          );
+        })}
+      </section>
+      <section>
+        <button className="block accent" onClick={exportJSON}>
+          Export JSON
+        </button>
+        <button className="block accent" onClick={importJSON}>
+          Import JSON
+        </button>
+      </section>
     </>
+  );
+}
+
+function CheckboxWithLabel({
+  label,
+  checked,
+  onChange,
+}: {
+  label: string;
+  checked: boolean;
+  onChange: () => void;
+}) {
+  return (
+    <div className="block" onClick={onChange}>
+      <input type="checkbox" checked={checked} readOnly />
+      <label>{label}</label>
+    </div>
   );
 }
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+export interface Keybind {
+  action: string;
+  bind: Bind;
+  category: string;
+}
+
+export interface Bind {
+  super: boolean;
+  ctrl: boolean;
+  alt: boolean;
+  shift: boolean;
+  key: string;
+}
